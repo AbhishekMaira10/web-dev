a1e39dd2a4803e9ab7defe7f333549b97fe86f81
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-06-15 03:06:53 +0530

    updated app.js and added notes.docx

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -53,4 +53,91 @@ html = `
   </ul>
 `;
 
+// ternary operator
+const id = 100;
+console.log(id === 100 ? "Correct" : "Incorrect");
 
+//switch case operator
+const color = "red";
+switch (color) {
+  case "red":
+    console.log("color is red");
+    break;
+  case "blue":
+    console.log("color is blue");
+    break;
+  default:
+    console.log("color is not red or blue");
+    break;
+}
+
+// Function Expressions
+
+const square = function (x = 3) {
+  return x * x;
+};
+
+console.log(square());
+
+// Immidiately invokable function expressions- IIFEs
+(function (name) {
+  console.log("Hello " + name);
+})("Brad");
+
+// Property methods
+
+const todo = {
+  add: function () {
+    console.log("Add todo");
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  },
+};
+
+todo.delete = function () {
+  console.log("Delete Todo");
+};
+
+// todo.add();
+// todo.edit(5);
+todo.delete();
+
+cars = ["Ford", "Chevy", "Honda", "Toyota"];
+
+// iterating via for loop
+
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
+
+// iterating via forEach method
+
+cars.forEach(function (car, index) {
+  console.log(`${index} : ${car}`);
+});
+
+//Map
+
+const Users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Brad" },
+  { id: 3, name: "Karen" },
+];
+
+const ids = Users.map(function (user) {
+  return user.id;
+});
+
+//Confirm
+
+if (confirm("Are you sure")) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+/*if the value of var is updated in a block the it gets updated in the global scope as well but not same
+for function scope 
+the value of let and const does not change outside the block or function scope
+*/
