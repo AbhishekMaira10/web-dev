35bf7e79e5eb403e01797b6040d059c3ebbdde3c
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-09-27 12:54:50 +0530

    add callback.js code

---
+++ JavaScript/async javascript/callback.js
@@ -0,0 +1,24 @@
+const posts = [
+  { title: "Post one", body: "This is post one" },
+  { title: "Post two", body: "This is post two" },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+getPosts();
+createPost({ title: "Post three", body: "This is post three" }, getPosts);
