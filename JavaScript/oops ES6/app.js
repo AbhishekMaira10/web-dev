3e8861f72f11c2c02135a83706f1e156dbec3c64
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-11-09 12:12:23 +0530

    update file

--- JavaScript/oops ES6/app.js
+++ JavaScript/oops ES6/app.js
@@ -20,7 +20,25 @@ class Person {
   }
 }
 
-const mary = new Person("Mary", "Johnson", "05-14-1999");
-console.log(mary.greeting());
+// const mary = new Person("Mary", "Johnson", "05-14-1999");
+// console.log(mary.greeting());
 
-console.log(Person.addNumbers(1, 2));
+// console.log(Person.addNumbers(1, 2));
+
+// Sub clasess
+
+class Customer extends Person {
+  constructor(firstName, lastName, dob, phone, membership) {
+    super(firstName, lastName, dob);
+
+    this.phone = phone;
+    this.membership = membership;
+  }
+
+  static getMembershipCost() {
+    return 500;
+  }
+}
+
+const john = new Customer("John", "Doe", "07-07-1977", "555-666-444", "Standard");
+console.log(john);
