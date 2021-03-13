f1bbadef473b9d94ce3af3a3666b286e894e6d2d
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2021-03-13 16:46:51 +0530

    Spread in function calls

--- JavaScript/Colt Steel Bootcamp/Newer JS Features/app.js
+++ JavaScript/Colt Steel Bootcamp/Newer JS Features/app.js
@@ -2,3 +2,9 @@
 function rollDice(numSides = 6){ // Return a value between 1 and numSides
 return Math.floor(Math.random()*numSides) + 1;
 }
+
+// Spread in function calls
+const nums = [1, 2, 4, 65, 76, 5646];
+Math.max(...nums); // output -  5646
+
+console.log(...nums) // output - 1 2 4 65 76 5646
