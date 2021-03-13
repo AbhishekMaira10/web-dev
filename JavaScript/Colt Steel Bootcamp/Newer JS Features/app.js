8a29d7f0e2646d9b71db93cb513d31da3d0a3d99
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2021-03-13 18:10:59 +0530

    Spread in array literals

--- JavaScript/Colt Steel Bootcamp/Newer JS Features/app.js
+++ JavaScript/Colt Steel Bootcamp/Newer JS Features/app.js
@@ -8,3 +8,9 @@ const nums = [1, 2, 4, 65, 76, 5646];
 Math.max(...nums); // output -  5646
 
 console.log(...nums) // output - 1 2 4 65 76 5646
+
+// Spread in Array litertals
+const cats = ['Blue', 'Scout', 'Rocket'];
+const dogs = ['Rusty', 'Wyatt']
+
+const allPets = [...cats, ...dogs];
